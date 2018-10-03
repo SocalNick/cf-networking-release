4512e96395868292a8f95782205a40ecca4a7c5f
Tyler Schultz <tschultz@pivotal.io>
2018-10-03 16:55:11 -0700

    WIP - happy path works, some error cases unhandled
    - when metadata does not exist
    - when destination (iprange) does not exist
    - various errors are not handled
    
    Signed-off-by: Michael Oleske <moleske@pivotal.io>

--- src/policy-server/integration/external_api_destinations_test.go
+++ src/policy-server/integration/external_api_destinations_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/onsi/gomega/gexec"
 )
 
-var _ = Describe("External Destination API", func() {
+var _ = FDescribe("External Destination API", func() {
 	var (
 		sessions          []*gexec.Session
 		policyServerConfs []config.Config
@@ -130,6 +130,20 @@ var _ = Describe("External Destination API", func() {
 			By("attempting to duplicate destinations")
 			_, err = client.CreateDestinations(token, toBeCreated...)
 			Expect(err).To(MatchError(MatchRegexp("http status 400.*entry with name 'tcp ips only' already exists")))
+
+			By("updating one of the destinations")
+			destToUpdate := createdDestinations[1]
+			destToUpdate.Name = "new name"
+			destToUpdate.Ports[0].End = 8080
+			updatedDests, err := client.UpdateDestinations(token, destToUpdate)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(updatedDests).To(HaveLen(1))
+			Expect(updatedDests[0]).To(Equal(destToUpdate))
+
+			By("listing all destinations and confirming that the update was persisted")
+			listedDestinations, err = client.ListDestinations(token)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(listedDestinations).To(ConsistOf(createdDestinations[0], updatedDests[0], createdDestinations[2]))
 
 			Eventually(fakeMetron.AllEvents, "5s").Should(ContainElement(
 				HaveName("DestinationsIndexRequestTime"),
